3b8377639445bc817634b9050ba70b764978a751
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-05-18 14:01:16 +0100

    Updating versions for mc1.21.5

---
+++ remappedSrc/io/github/sandydunlop/newsfeed/NewsfeedModInitializer.java
@@ -0,0 +1,15 @@
+package io.github.sandydunlop.newsfeed;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class NewsfeedModInitializer implements ModInitializer {
+	public static final String MOD_ID = "newsfeed";
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+	}
+}
