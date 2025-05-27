e3c39960692c211cb9174de23ba69e65834d9f76
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-05-27 15:59:54 +0100

    Moving events to their own place

---
+++ src/client/java/io/github/sandydunlop/cupra/gui/events/CListBoxSelectionChangedEvent.java
@@ -0,0 +1,10 @@
+package io.github.sandydunlop.cupra.gui.events;
+
+import java.util.EventObject;
+
+
+public class CListBoxSelectionChangedEvent extends EventObject {
+    public CListBoxSelectionChangedEvent(Object source) {
+        super(source);
+    }
+}
--- src/client/java/io/github/sandydunlop/newsfeed/ListBoxSelectionChangedListener.java
+++
@@ -1,8 +0,0 @@
-package io.github.sandydunlop.newsfeed;
-
-import java.util.EventListener;
-
-
-public interface ListBoxSelectionChangedListener extends EventListener {
-    void selectionChanged(ListBoxSelectionChangedEvent event);
-}
