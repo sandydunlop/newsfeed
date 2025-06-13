33ff6680d587c01d0f7626bc5def0ba5e6f6d1c0
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-06-13 14:24:54 +0100

    Updating version number

--- src/client/java/io/github/sandydunlop/newsfeed/Ticker.java
+++ src/client/java/io/github/sandydunlop/newsfeed/Ticker.java
@@ -91,6 +91,7 @@ public class Ticker {
 
     public void render() {
         if (segments != null && !segments.isEmpty()){
+    		context.fill(0, 0, context.getScaledWindowWidth(), 32, 0x77000000);
             int x = -scrollAmount;
             for (int i=0; i<segments.size(); i++) {
                 try{
