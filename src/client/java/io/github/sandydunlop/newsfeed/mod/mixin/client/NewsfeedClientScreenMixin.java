bbe0bb739507e284b51694ace1e0deacbd11d7df
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-07-02 13:44:22 +0100

    keybinding

--- src/client/java/io/github/sandydunlop/newsfeed/mod/mixin/client/NewsfeedClientScreenMixin.java
+++ src/client/java/io/github/sandydunlop/newsfeed/mod/mixin/client/NewsfeedClientScreenMixin.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import io.github.sandydunlop.cupra.common.CupraScreen;
+import io.github.sandydunlop.cupra.platform.minecraft.MinecraftServices;
+import io.github.sandydunlop.newsfeed.app.MainScreen;
 import io.github.sandydunlop.newsfeed.mod.ModMainScreen;
 import io.github.sandydunlop.newsfeed.mod.NewsfeedClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -20,7 +23,8 @@ public abstract class NewsfeedClientScreenMixin {
 	@Inject(method = "keyPressed", at = @At(value = "HEAD"), cancellable = true)
 	public void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> info) {
 		if (keyCode == KeyBindingHelper.getBoundKeyOf(NewsfeedClientModInitializer.newsfeedKeyBind).getCode()) {
-			if ((Object)this instanceof ModMainScreen){
+			CupraScreen screen = MinecraftServices.getInstance().getApp().getScreen();
+			if (screen instanceof MainScreen) {
 				NewsfeedClientModInitializer.newsfeedKeyBind.setPressed(false);
 				info.setReturnValue(true);
 				this.close();
